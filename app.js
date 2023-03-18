fe2589e9a7e24e002a07dcd2b0db12e154ad25b9
Dmitriy Domrachev <dimd1288@gmail.com>
2023-03-18 08:59:24 +0300

    refactoring

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { celebrate, Joi, errors } = require('celebrate');
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
+const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/error-handler');
-const NotFoundError = require('./errors/not-found-error');
+const routes = require('./routes/index');
 
 const app = express();
 
@@ -17,29 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email({ tlds: { allow: false } }),
-    password: Joi.string().required(),
-  }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/^(https?:\/\/.)[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$/),
-    email: Joi.string().required().email({ tlds: { allow: false } }),
-    password: Joi.string().required(),
-  }),
-}), createUser);
-
-app.use('/users', auth, require('./routes/users'));
-app.use('/cards', auth, require('./routes/cards'));
-
-app.all('*', auth, () => {
-  throw new NotFoundError('Wrong url');
-});
+app.use(routes);
 
 app.use(errors());
 
